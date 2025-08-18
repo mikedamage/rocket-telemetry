3a4c6fe89dd76b39c11d8aab49e4990860840666
Mike Green <mike.green@iheartjane.com>
2025-08-18 17:24:28 -0400

    telemetry encoded as msgpack over UDP, batches of 20, working correctly

--- telemetry_server/udp-server.js
+++ telemetry_server/udp-server.js
@@ -3,12 +3,19 @@ import dgram from "node:dgram";
 import { log } from "./lib/logger.js";
 import path from "node:path";
 import { createWriteStream } from "node:fs";
+import { decode } from "@msgpack/msgpack";
+import { format as formatCsv } from "@fast-csv/format";
 
 const port = process.env.PORT || 5150;
 const fileName = process.env.FILE || "output.csv";
 const server = dgram.createSocket("udp4");
 const csvFile = path.resolve(path.join(import.meta.dirname, fileName));
-const telemetryStream = createWriteStream(csvFile, { flags: "a" });
+const telemetryStream = createWriteStream(csvFile, { flags: "w" });
+const formatter = formatCsv({
+  headers: true,
+});
+
+formatter.pipe(telemetryStream);
 
 server.on("listening", () => {
   log(`UDP server listening on port ${port}`);
@@ -20,9 +27,22 @@ server.on("error", (err) => {
 });
 
 server.on("message", (msg, rinfo) => {
-  log(`Received ${msg.length} bytes from ${rinfo.address}:${rinfo.port}`);
-  log(msg.toString().trim());
-  telemetryStream.write(msg.toString().trim() + "\n");
+  log(`Received ${rinfo.size} bytes from ${rinfo.address}:${rinfo.port}`);
+  // console.log(msg.toString());
+  try {
+    // log(msg.toString().trim());
+    // telemetryStream.write(msg.toString().trim() + "\n");
+    const unpacked = decode(msg);
+
+    for (const row of unpacked.readings) {
+      formatter.write(row);
+    }
+
+    log(`${telemetryStream.bytesWritten} bytes written so far`);
+  } catch (err) {
+    log("error decoding msgpack:");
+    console.error(err);
+  }
 });
 
 server.on("close", () => {
